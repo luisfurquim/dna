b24fd1bbc0fe23a77d3a118f3b47b61684a307dd
Luis Otavio de Colla Furquim <luisfurquim@gmail.com>
2023-09-14 17:02:53 -0300

    @{debug}

---
+++ Dna.DumpSpec.go
@@ -0,0 +1,29 @@
+package dna
+
+import (
+	"fmt"
+)
+
+func (d *Dna) DumpSpec(tabName string) string {
+	var list *list
+	var ruleName string
+	var join int
+	var rule tabRule
+	var dump string
+	var ok bool
+
+	if _, ok = d.list[tabName]; ok {
+		for ruleName, list = range d.list[tabName] {
+			dump += "Rule name: %s\n" + ruleName
+			for join, rule = range list.joins {
+				dump += fmt.Sprintf("%d: %#v\n", join, rule)
+			}
+			dump += "\n"
+		}
+	}
+
+	return dump
+}
+
+
+
