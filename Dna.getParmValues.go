799590066353e83039e60d50589ef694a6370faf
Luis Otavio de Colla Furquim <luisfurquim@gmail.com>
2024-06-24 02:34:44 -0300

    @{debug}

--- Dna.getParmValues.go
+++ Dna.getParmValues.go
@@ -12,7 +12,7 @@ func (d *Dna) getParmValues(tabName string, refRow reflect.Value, recursive func
 
 	parms = make([]interface{}, 0, len(d.tables[tabName].fields))
 	for _, fld = range d.tables[tabName].fields {
-//		Goose.Query.Logf(0, "fld:%#v", fld)
+		Goose.Query.Logf(5, "fld:%#v", fld)
 		if fld.JoinList { //aqui
 			fk = refRow.Field(fld.Index)
 			if isNonEmptySlice(fk) {
@@ -21,28 +21,28 @@ func (d *Dna) getParmValues(tabName string, refRow reflect.Value, recursive func
 		}
 
 		if fld.Fk != "" {
-//			Goose.Query.Logf(0, "fld.fk:%#v", fld.fk)
+			Goose.Query.Logf(5, "fld.fk:%#v", fld.fk)
 			fk = refRow.Field(fld.Index)
-//			Goose.Query.Logf(0, "fk:%#v", fk)
+			Goose.Query.Logf(5, "fk:%#v", fk)
 			if !fk.IsValid() || fk.IsNil() || fk.IsZero() {
-//				Goose.Query.Logf(0, "!valid")
+				Goose.Query.Logf(5, "!valid")
 				parms = append(parms, 0)
 			} else {
-//				Goose.Query.Logf(0, "recursive?")
+				Goose.Query.Logf(5, "recursive?")
 				if recursive != nil {
-//					Goose.Query.Logf(0, "yes")
+					Goose.Query.Logf(5, "yes")
 					recursive(fk.Interface())
 				}
 				
 				fk = fk.Elem().Field(d.tables[fld.Fk].pkIndex)
-//				Goose.Query.Logf(0, "++fk:%#v", fk)
+				Goose.Query.Logf(5, "++fk:%#v", fk)
 				parms = append(parms, fk.Interface())
 			}
 		} else {
 			if !fld.PK {
 				parms = append(parms, refRow.Field(fld.Index).Interface())
 			}
-//			Goose.Query.Logf(0, "parms:%#v", parms)
+			Goose.Query.Logf(5, "parms:%#v", parms)
 		}
 	}
 
